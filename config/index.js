6d55e4c26d1af96dd2068906e8c75c0ab0184604
Franco <pogui@Francos-MacBook-Pro.local>
2015-08-04 14:59:15 -0300

    added production config file

--- config/index.js
+++ config/index.js
@@ -1,7 +1,9 @@
 var nodeEnv = process.env.NODE_ENV || 'development';
 var config = {
   				development: require('./development'),
-  				test: require('./test')
+  				test: require('./test'),
+  				production: require('./production')
+
 	};
 
 module.exports = config[nodeEnv];
