daac41a2f6ba5e7bf3b7e1cd8a09399eeabde4e2
Franco <pogui@Francos-MacBook-Pro.local>
2015-09-09 09:38:24 -0300

    cosas varias

--- db/models/messages.js
+++ db/models/messages.js
@@ -2,12 +2,10 @@
 * pogui @ tween (6/2015)
 * User model funtions.
 */
-var ObjectId = require('mongodb').ObjectID;
-
 
 module.exports.getById = function(id, cb) {
   var collection = db.collection('messages');
-  collection.find({'_id': new ObjectId(id)}).toArray(function(err, items) {
+  collection.find({msgId: id}).toArray(function(err, items) {
     (items.length > 0)?cb(items[0].token):cb(false);
    });
 }
