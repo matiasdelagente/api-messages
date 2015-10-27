a68bd94fada077e1fb17d070fb05aecf47e159e4
Matias Sassi <matias.sassi@tween.com.ar>
2015-10-27 11:48:10 -0300

    fixed typo error in message status property name

--- middleware/index.js
+++ middleware/index.js
@@ -16,7 +16,7 @@ module.exports.message = function(req,res,next) {
 
 module.exports.update = function(req,res,next) {
 	var message = req.body;
-	if(message.msgStatus == undefined || Math.abs(message.msgStatus) > 5)
- 		res.status(422).send({ type: 'Unprocessable request',description: 'Missing/malformed msgStatus'});
+	if(message.status == undefined || Math.abs(message.status) > 5)
+ 		res.status(422).send({ type: 'Unprocessable request',description: 'Missing/malformed status'});
 	else next();
 }
