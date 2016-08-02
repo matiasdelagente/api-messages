545998ba24335330a004391dd0ec3f272c5e22eb
Matias P. Sassi <matias.sassi@tween.com.ar>
2016-08-02 15:18:38 -0300

    added response

--- routes/callbacks.js
+++ routes/callbacks.js
@@ -7,22 +7,22 @@ module.exports.infobip = function(req, res)
 {
   var dlr = req.body;
   dlr.smsSender = "infobip";
+  res.status(200).send({"status": "OK"});
   rabbit.sendToCallbackExchange(req.body, "i");
-  res.status(200).send();
 }
 
 module.exports.nexmo = function(req, res)
 {
   var dlr = req.query;
   dlr.smsSender = "nexmo";
+  res.status(200).send({"status": "OK"});
   rabbit.sendToCallbackExchange(req.body, "x");
-  res.status(200).send();
 }
 
 module.exports.clickatell = function(req, res)
 {
   var dlr = req.body;
   dlr.smsSender = "clickatell";
+  res.status(200).send({"status": "OK"});
   rabbit.sendToCallbackExchange(req.body, "t");
-  res.status(200).send();
 }
