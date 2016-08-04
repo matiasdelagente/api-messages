0f0153f26844b53d716f02d8d8c64a5ad1dff493
Matias P. Sassi <matias.sassi@tween.com.ar>
2016-08-04 09:53:57 -0300

    fixed amqp binding errors and dlr to amqp publishing

--- helpers/amqp.js
+++ helpers/amqp.js
@@ -19,17 +19,17 @@ module.exports.setupAMQP = function(ch, amqpConfig, assertCallback)
     // the exchange->queue bindings
     bindings = JSON.stringify(exchangeData.bindings);
     routingKey = "";
-    for(var queueBinding in bindings.queues)
+    for(var queueBinding in exchangeData.bindings.queues)
     {
-      bindingData = bindings.queues[queueBinding];
+      bindingData = exchangeData.bindings.queues[queueBinding];
       routingKey = typeof bindingData.routingKey !== "undefined" ? bindingData.routingKey : "";
-      ch.bindQueue(bindingData.name, exchangeData.name, routingKey, assertCallback);
+      ch.bindQueue(bindingData.name, exchangeData.name, routingKey, {}, assertCallback);
     }
     // the exchange->exchange bindings
-    for(var exchangeBinding in bindings.exchanges)
+    for(var exchangeBinding in exchangeData.bindings.exchanges)
     {
       bindingData = exchangeData.bindings.queues[exchangeBinding];
-      ch.bindExchange(bindingData.name, exchangeData.name, bindingData.routingKey, assertCallback);
+      ch.bindExchange(bindingData.name, exchangeData.name, bindingData.routingKey, {}, assertCallback);
     }
   }
 }
